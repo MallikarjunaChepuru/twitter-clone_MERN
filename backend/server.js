eafb25ede979b2b266bbcb333550919ba6144d51
MallikarjunaChepurue <mallich178@gmail.com>
2024-10-02 19:26:04 +0530

    1.auth-controler-used

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 dotenv.config();
 import authRoutes from "./routes/auth.routes.js";
-import connectDB from "./controllers/db.js";
+import connectDB from "./config/db.js";
 const app = express();
 
 const PORT = process.env.PORT || 3012;
-app.use(express.json());
-
+app.use(express.json()); // to parse req.body
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
