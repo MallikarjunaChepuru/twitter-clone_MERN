08052e18aaa4867b5249940e3054f967f60e951f
MallikarjunaChepurue <mallich178@gmail.com>
2024-10-05 22:07:29 +0530

    1.user routes and controllers added

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-dotenv.config();
+import { v2 as cloudinary } from "cloudinary";
+
 import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import connectDB from "./config/db.js";
+
 const app = express();
+dotenv.config();
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const PORT = process.env.PORT || 3012;
 app.use(express.json()); // to parse req.body
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
+
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   connectDB();
